d77d955e3ecd80a9ce9a5bfd86cd5963199247f7
ulasislak <ulasislak06@gmail.com>
2025-03-25 14:31:09 +0300

    login sayfası ve admin controller oluşturuldu.

---
+++ OrderAndManagementApp/Controllers/OwnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderAndManagementApp.Controllers
+{
+    public class OwnerController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
