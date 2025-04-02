535bfc48b7cdfece1766c78f8ef18f1d4104f658
ulasislak <ulasislak06@gmail.com>
2025-04-02 20:11:45 +0300

    home içindeki ürünler sayfası oluşturuldu.işlevsel hale geldi.

--- OrderAndManagementApp/Controllers/HomeController.cs
+++ OrderAndManagementApp/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 using System.Diagnostics;
+using AutoMapper;
+using BusinnesLogic.AbstractServices;
 using Microsoft.AspNetCore.Mvc;
 using OrderAndManagementApp.Models;
+using OrderAndManagementApp.ViewModel;
 
 namespace OrderAndManagementApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productService;
+    private readonly IMapper _mapper;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,IProductService productService,IMapper mapper)
     {
         _logger = logger;
+        _productService = productService;
+        _mapper = mapper;
     }
 
     public IActionResult Index()
@@ -28,4 +35,13 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Products()
+    {
+        var GetAll = await _productService.GetAllProduct();
+        var AllProduct = _mapper.Map<List<ProductVM>>(GetAll);
+        return View(AllProduct);
+    }
+
 }
